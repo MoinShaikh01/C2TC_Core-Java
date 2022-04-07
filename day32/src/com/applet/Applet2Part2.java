e9812ec45d973fb8bb2cc45f40b6429d099b2325
Moin Shaikh <Moin Shaikh@MoinShaikh-PC>
2022-04-08 00:34:57 +0530

    first commit

---
+++ day32/src/com/applet/Applet2Part2.java
@@ -0,0 +1,29 @@
+package com.applet;
+
+import java.applet.Applet;
+import java.awt.Button;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Applet2Part2 extends Applet implements ActionListener{
+	Button b1,b2;
+	TextField t;
+	
+	public void init() {
+		add(b1 = new Button("Click"));
+		add(t = new TextField(15));
+		add(b2 = new Button("Clear"));
+		b1.addActionListener(this);
+		b2.addActionListener(new A());
+	}
+	public void actionPerformed(ActionEvent arg0) {
+		t.setText("Hello World");
+	}
+	class A implements ActionListener {
+		
+		public void actionPerformed(ActionEvent e) {
+			t.setText("");
+	}
+  }
+}
