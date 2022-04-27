33d3a23f5d28a362f49809ca2e336364e8b21c27
Moin Shaikh <Moin Shaikh@MoinShaikh-PC>
2022-04-28 01:19:05 +0530

    first commit

---
+++ Day52/src/thread/ExceptionDemo.java
@@ -0,0 +1,15 @@
+package thread;
+
+public class ExceptionDemo {
+	public void m1() throws Exception {
+			
+		}
+}
+
+class Qt {
+	
+	public void m1() throws Exception {
+		ExceptionDemo ed = new ExceptionDemo();
+		ed.m1();
+	}
+}
